f653aa8d765ff6b25f830a47c99acdfd7eb747d8
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-04-29 09:02:13 +0530

    Add welcome images for homepage.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import ProductList from "./components/home/ProductList";
 import './assets/stylesheets/main.scss'
 import DarkModeToggle from "./components/DarkModeToggleDiv";
 import SearchBar from "./components/home/SearchBar";
+import Welcome from "./components/home/Welcome";
 
 function App() {
     return (
         <div className="App">
             <DarkModeToggle/>
+            <Welcome/>
             <SearchBar/>
             <ProductList/>
         </div>
