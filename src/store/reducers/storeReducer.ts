599de06cc0a766adcb1b621241eb79da965296bc
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-04-30 14:19:17 +0530

    remove hard coded product list.

--- src/store/reducers/storeReducer.ts
+++ src/store/reducers/storeReducer.ts
@@ -7,35 +7,12 @@ import {
     UPDATE_PRODUCT,
 } from "../../constants/storeConstants";
 
-import bun1 from '../../assets/images/products/bun1.jpg';
-import bun2 from '../../assets/images/products/bun2.jpg';
-import bun3 from '../../assets/images/products/bun3.jpg';
-import bun4 from '../../assets/images/products/bun4.jpg';
-import bun5 from '../../assets/images/products/bun5.jpg';
-import cake1 from '../../assets/images/products/cake1.jpg';
-import cake2 from '../../assets/images/products/cake2.jpg';
-import cake3 from '../../assets/images/products/cake3.jpg';
-import cake4 from '../../assets/images/products/cake4.jpg';
-import cake5 from '../../assets/images/products/cake5.jpg';
-
 const initialState: OnlineStoreState = {
-    products: [
-        {title: "Bun-1", sellPrice: 40, price: 45, image: bun1, cType: 'Bakery', stockQty: 10},
-        {title: "Bun-2", sellPrice: 40, price: 45, image: bun2, cType: 'Bakery', stockQty: 10},
-        {title: "Bun-3", sellPrice: 40, price: 45, image: bun3, cType: 'Bakery', stockQty: 10},
-        {title: "Bun-4", sellPrice: 40, price: 45, image: bun4, cType: 'Bakery', stockQty: 10},
-        {title: "Bun-5", sellPrice: 40, price: 45, image: bun5, cType: 'Bakery', stockQty: 10},
-        {title: "cake-1", sellPrice: 40, price: 45, image: cake1, cType: 'Bakery', stockQty: 10},
-        {title: "cake-2", sellPrice: 40, price: 45, image: cake2, cType: 'Bakery', stockQty: 10},
-        {title: "cake-3", sellPrice: 40, price: 45, image: cake3, cType: 'Bakery', stockQty: 10},
-        {title: "cake-4", sellPrice: 40, price: 45, image: cake4, cType: 'Bakery', stockQty: 40},
-        {title: "cake-5", sellPrice: 40, price: 45, image: cake5, cType: 'Bakery', stockQty: 40},
-    ],
+    products: [],
     searchTerm: '',
     isDarkTheme: true,
     isLogged: false,
     selectedCategory: 'All',
-
 }
 
 export const OnlineStoreReducer =
